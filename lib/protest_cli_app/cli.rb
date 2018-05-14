436d63c75b962b7c73d78c09ca49778a69a5eec1
ode-to-the-code <justcallmethis@gmail.com>
2018-05-14 16:28:22 +0700

    changed some text to clean up CLI menu output

--- lib/protest_cli_app/cli.rb
+++ lib/protest_cli_app/cli.rb
@@ -7,14 +7,14 @@ class ProtestCliApp::CLI
 
   def call
     # binding.pry
-    puts "hello world"
+    # puts "hello world"
     list_books
     menu
     goodbye
   end
 
   def list_books
-    puts "here are some bestselling books"
+    puts "here are some bestselling books!"
 
     @books = ProtestCliApp::Protest.today
     @books.each.with_index(1) do |book, i|
@@ -26,13 +26,16 @@ class ProtestCliApp::CLI
     input = nil
     while input != "exit"
       puts "enter the number of the book you're interested in, or type list to see
-            the list again, or type exit to exit the program"
+            the list again, or type exit to exit the program."
       input = gets.strip.downcase
 
     if input.to_i > 0
       puts @books[input.to_i - 1].description
     elsif input == "list"
       list_books
+    elsif input == "exit"
+      goodbye
+      exit
     else
       puts "not sure what you want, type list or exit."
     end
@@ -40,7 +43,7 @@ class ProtestCliApp::CLI
   end
 
   def goodbye
-    puts "see you tomorrow for more books"
+    puts "see you tomorrow for more books!"
   end
 
 
